909f47122903dbcfe3f0156e6803a73039014fd6
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-01-15 17:56:31 +0900

    [FEAT] POST API 단순 텍스트 요청 예제

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -16,32 +16,32 @@ import java.util.Enumeration;
 public class RequestParamServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("[전체 파라미터 조회] - start");
 
-        req.getParameterNames().asIterator()
+        request.getParameterNames().asIterator()
                         .forEachRemaining(
-                                paramName -> System.out.println(paramName + "=" + req.getParameter(paramName))
+                                paramName -> System.out.println(paramName + "=" + request.getParameter(paramName))
                         );
 
         System.out.println("[전체 파라미터 조회] - end");
         System.out.println();
 
         System.out.println("[단일 파라미터 조회]");
-        String username = req.getParameter("username");
-        String age = req.getParameter("age");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
 
         System.out.println("username = " + username);
         System.out.println("age = " + age);
         System.out.println();
 
         System.out.println("[이름이 같은 복수 파라미터 조회]");
-        String[] usernames = req.getParameterValues("username");
+        String[] usernames = request.getParameterValues("username");
         for (String name : usernames) {
             System.out.println("username = " + name);
         }
 
-        resp.getWriter().write("ok");
+        response.getWriter().write("ok");
 
 
     }
