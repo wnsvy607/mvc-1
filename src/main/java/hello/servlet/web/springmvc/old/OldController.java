5c52e391bff0b4e40a6a45717b96d006d268358c
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-02-03 10:44:29 +0900

    [FEAT] Controller 인터페이스에 뷰 리졸버 사용

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -10,10 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
 
-
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
