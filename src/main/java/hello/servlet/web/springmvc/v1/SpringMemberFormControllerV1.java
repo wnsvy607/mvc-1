b421d10217de6aeda6acf54a1381d4c4af61a11a
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-02-04 21:29:28 +0900

    [FEAT] 스프링 MVC 패턴 적용

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+// 아래 두 어노테이션이 있으면 핸들러로 매핑이 된다 (RequestMappingHandlerMapping)
+//@Component
+//@RequestMapping
+
+// 컨트롤러 어노테이션은 둘 다를 포함하고 있다.
+@Controller
+
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
